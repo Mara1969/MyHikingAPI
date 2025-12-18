951fdec34505e1de2033059fe298c65b1f499ab2
Mara1969 <mariyahsajjad@gmail.com>
2025-12-18 15:09:14 +0000

    refactoring

--- My-Hiking-API/Services/DatabaseService.cs
+++ My-Hiking-API/Services/DatabaseService.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Options;
 using MyHikingAPI.Services;
 using MyHikingAPI.Models.Configuration;
 using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using MyHikingAPI.Models;
 
 namespace MyHikingAPI.Services
 {
@@ -15,12 +17,18 @@ namespace MyHikingAPI.Services
             _settings = options.Value;
         }
         
-        // Connect to the database 
-        public DbConnection CreateConnection()
+        public List<Mountain> GetMountainNames()
         {
-        using (var connection = new SqlConnection(_settings.ConnectionString)) 
-        {    
-        // Create a query that retrieves all books with an author name of "John Smith"    
+            var mountainNames = new List<Mountain>();
+
+            // Connect to the database 
+
+            using (var connection = new SqlConnection(_settings.ConnectionString))
+            {
+                // Create queries to retrieve mountain data from the database 
+            }
+
+            return mountainNames;
          
         }
     }
