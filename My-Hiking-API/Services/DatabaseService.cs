866d420dd74066971615ac7ef36fa02a76f35eea
Mara1969 <mariyahsajjad@gmail.com>
2025-12-19 12:44:28 +0000

    adding methods to read and insert mountain data into database

--- My-Hiking-API/Services/DatabaseService.cs
+++ My-Hiking-API/Services/DatabaseService.cs
@@ -5,6 +5,7 @@ using MyHikingAPI.Models.Configuration;
 using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using MyHikingAPI.Models;
+using Dapper;
 
 namespace MyHikingAPI.Services
 {
@@ -14,21 +15,40 @@ namespace MyHikingAPI.Services
 
         public DatabaseService(IOptions<SqlDatabaseOptions> options)
         {
-            _settings = options.Value;
+            _settings = options.Value; // contains the bound ConnectionString 
         }
-        
-        public List<Mountain> GetMountainNames()
+
+        // Method to insert values into the mountain table in the database
+         public void InsertMountains(List<Mountain> mountains)
         {
-            var mountainNames = new List<Mountain>();
+            // using Dapper for simplified data insertion
+            const string sql = @"
+                INSERT INTO Mountains (Id, Name, Height)
+                VALUES (@Id, @Name, @Height);"; // act as plaeholders for the actual values
+
+            using (var connection = new SqlConnection(_settings.ConnectionString))
+            {
+                    
+                connection.Execute(sql, mountains);
+                
+            }
+        }
+
+        // Method to read from the database
+        public List<Mountain> GetAllMountains()
+        {
+            // using Dapper to simplify data retrieval
+            const string sql = @"
+                SELECT Id, Name, Height
+                FROM Mountains;
+            ";
 
             // Connect to the database 
 
             using (var connection = new SqlConnection(_settings.ConnectionString))
             {
-                // Create queries to retrieve mountain data from the database 
+                return connection.Query<Mountain>(sql).AsList();
             }
-
-            return mountainNames;
          
         }
     }
