6c6ec09ce4efe8a8afd334643606c4058126a5c6
Mara1969 <mariyahsajjad@gmail.com>
2025-12-22 12:17:40 +0000

    injecting the database service and including method to get all mountains data from the database

--- My-Hiking-API/Services/MountainService.cs
+++ My-Hiking-API/Services/MountainService.cs
@@ -1,15 +1,31 @@
 using System.Collections.Generic;
 using MyHikingAPI.Services;
 using MyHikingAPI.Models;
+using System.Threading.Tasks;
 
 
 namespace MyHikingAPI.Services
 {
     public class MountainService :  IMountainService
     {
+        private readonly IDatabaseService _databaseService; // Dependency on database service
+
+        // Constructor with dependency injection. The IDatabaseService is passed into the constructor by the DI container 
+        public MountainService(IDatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
         public List<Mountain> GetAllMountains()
         {
             return JsonReader.GetData<Mountain>("Data/mountains.json");
-        }               
+        }      
+        public async Task<List<Mountain>> GetAllMountainsFromDb()
+        {
+            const string sql = @"
+            SELECT Id, Name, Height 
+            FROM Mountains;";
+
+            return await _databaseService.GetDataEntries<Mountain>(sql);
+        }         
     }
 }
